f7713031433ca9c640f09b5bb53d20a55c834303
fezebr <fezebr2000@gmail.com>
2021-02-17 12:25:06 +0330

    add form and simple css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-content">
+        <header>
+          <h1>form validations</h1>
+        </header>
+        <form >
+          <div className="input-group">
+            <p className="label">Full Name</p>
+            <input placeholder="full Name" className="text-button" />
+            <p className="error">error</p>
+          </div>
+          <div className="input-group">
+            <p className="label">Email Adress</p>
+            <input placeholder="full Name" className="text-button" />
+            <p className="error">errr</p>
+          </div>
+          <div className="input-group">
+            <p className="label">Password</p>
+            <input placeholder="full Name" className="text-button" />
+            <p className="error">errr</p>
+          </div>
+
+          <div className="input-group checkbox">
+            <label className="label">accept roules</label>
+            <input type="checkbox" placeholder="full Name" className="text-button" />
+            <p className="error">errr</p>
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
